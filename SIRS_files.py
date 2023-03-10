553a50d64f44633a19facb1890603c0b9688c3f6
cristinaM57 <75209928+cristinaM57@users.noreply.github.com>
2023-03-10 02:58:29 +0000

    Add files via upload

--- SIRS_files.py
+++ SIRS_files.py
@@ -8,6 +8,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import pandas as pd
+import csv
+
+def writefile(lx, calc):
+    outfile = f"{calc}_Size{lx}.csv"
+    header = ['Infected_Fraction', 'Immune_Percent']
+    with open(outfile, "w", newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(header)
 
 def bootstrap(infected_array, lx, ly):
     L = len(infected_array)
@@ -16,7 +24,7 @@ def bootstrap(infected_array, lx, ly):
     infected_variances  = []
 
     for i in range(nsubsets):
-        indices = np.random.choice(L, int(L))
+        indices = np.random.randint(0,L, L)
         subset_infected = infected_array[indices]
 
         infected_variances.append(np.var(subset_infected)/(lx*ly))
@@ -25,7 +33,7 @@ def bootstrap(infected_array, lx, ly):
 
 def SIRS_phases(lx, ly, p1, p2, p3, spin):
 
-    nstep=1100 #discard first 100 sweeps
+    nstep=1200 #discard first 200 sweeps
     sweeps=0
     infected = []
     
@@ -93,7 +101,7 @@ def SIRS_phases(lx, ly, p1, p2, p3, spin):
 
     
     #save measurements to file after 1000 sweeps (disregard first 100)
-    n_del = 100
+    n_del = 200
     del infected[:n_del]
 
     inf_array = np.asarray(infected)
@@ -105,7 +113,7 @@ def SIRS_phases(lx, ly, p1, p2, p3, spin):
 
 def SIRS_var(lx, ly, p1, p2, p3, spin):
 
-    nstep=10500 #discard first 100 sweeps
+    nstep=10500 #discard first 500 sweeps
     sweeps=0
     infected = []
     
@@ -206,6 +214,86 @@ def immune(spin, lx, ly, per_immune):
 
     return spin
 
+def SIRS_immunity(lx, ly, p1, p2, p3, spin):
+
+    nstep=10500 #discard first 500 sweeps
+    sweeps=0
+    infected = []
+    
+    """
+    fig = plt.figure()
+    im=plt.imshow(spin, animated=True)
+    """
+    #update loop here - for SIRS
+
+    for n in range(nstep):
+
+        #create lists with 2500 random numbers
+        i_values = np.random.choice(lx, lx*ly)
+        j_values = np.random.choice(ly, lx*ly)
+        
+        for i in range(lx*ly):          
+    #select spin randomly
+            itrial = i_values[i]
+            jtrial = j_values[i]
+
+    #perform test
+            if spin[itrial, jtrial] == 0:
+                # check neighbours
+                if ((spin[(itrial+1)%lx,jtrial]) == -1) or ((spin[(itrial-1)%lx,jtrial]) == -1) or ((spin[itrial,(jtrial+1)%lx]) == -1) or ((spin[itrial,(jtrial-1)%lx]) == -1):
+                    x = random.random()
+                    #print("p1"+ str(x)) #debug
+                    if x <= p1:
+                        spin[itrial,jtrial] = -1  #you only need to change one spin, not update the whole thing
+                else:
+                    spin[itrial, jtrial] = 0
+            
+            elif spin[itrial, jtrial] == -1:
+                x = random.random()
+                #print("p2"+ str(x)) #debug
+                if x <= p2:
+                    spin[itrial,jtrial] = 1
+
+            elif spin[itrial, jtrial] == 1:
+                x = random.random()
+                #print("p3"+ str(x)) #debug
+                if x <= p3:
+                    spin[itrial,jtrial] = 0
+        
+            #print("i" + str(itrial) + " j" + str(jtrial)) #debug
+
+        #print(spin) #debug
+                
+        #occasionally plot or update measurements, eg every 10 sweeps
+        if (n%1==0): 
+
+            #calculate and save data every sweep
+            I = np.count_nonzero(spin == -1)
+            infected.append(I)
+            
+            """
+        #       show animation
+            plt.cla()
+            im=plt.imshow(spin, animated=True, vmin = -1, vmax =1)
+            plt.draw()
+            plt.pause(0.0001)
+            """
+
+            sweeps += 1
+            print(" sweeps:" + str(sweeps) + "/" + str(nstep), end="\r")
+
+    
+    #save measurements to file after 1000 sweeps (disregard first 100)
+    n_del = 500
+    del infected[:n_del]
+
+    inf_array = np.asarray(infected)
+    inf_avg = np.mean(infected)
+    inf_var = np.var(inf_array)/(lx*ly)
+    inf_frac = inf_avg/(lx*ly) 
+
+    return inf_frac, inf_var
+
 def main():
     
     #input
@@ -216,9 +304,9 @@ def main():
 
     if calc == "heatmap":
 
-        p1_arr = np.arange(0, 1.05, 0.05)
+        p1_arr = np.arange(0, 1, 0.05)
         p2 = 0.5
-        p3_arr = np.arange(0, 1.05, 0.05)
+        p3_arr = np.arange(0, 1, 0.05)
 
         size = len(p1_arr)
         
@@ -241,7 +329,7 @@ def main():
 
     elif calc == "variance":
 
-        p1_arr = np.arange(0.2, 0.51, 0.01)
+        p1_arr = np.arange(0.2, 0.50, 0.01)
         p2 = 0.5
         p3 = 0.5
 
@@ -269,7 +357,24 @@ def main():
         df2.to_csv(f'SIRSVAR_var_err_size{lx}', sep=' ', header = False, float_format='%.5f', index=False)
     
     elif calc == "immunity":
+
+        writefile(lx, calc)
+
+        p1 = 0.5
+        p2 = 0.5
+        p3 = 0.5
+
+        probs = np.arange(0, 1, 0.01)
         spin = np.random.choice([-1, 0, 1], [lx, ly])
-        print(immune(spin, lx, ly, 0.05))
+
+        for i in range(len(probs)):
+            prob = probs[i]
+            spin_update = immune(spin, lx, ly, prob)
+            x = (SIRS_immunity(lx, ly, p1, p2, p3, spin_update)[0])
+            outfile = f"{calc}_Size{lx}.csv"
+            data = [x, prob]
+            with open(outfile, "a", newline='') as f:
+                writer = csv.writer(f)
+                writer.writerow(data)
 
 main()
